310c3fecf25e5599480995d58671d5010d5e2e64
Lev Kowalski <levkowalski@gmail.com>
2023-05-20 15:53:59 +0200

    wip

---
+++ pages/bibliography.js
@@ -0,0 +1,12 @@
+
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Bibliogrpahy() {
+  return (
+    <div className="container">
+       hello
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="container">
@@ -15,6 +18,10 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <Link href="/expositions">Expositions</Link>
+        <h2>Contact</h2>
+        <a href="mailto:andrea.a.kowalski@gmail.com">Andrea Kowalski</a>
+        <a href="mailto:levkowalski@gmail.com">Lev Kowalski</a>
       </main>
 
       <Footer />
